cdffb8efcd249dd69c98d0314aaeb8148c27f44d
Maila Akhil <makhilbunny@gmail.com>
2026-08-04 18:46:47 +0530

    Added Number programs in java

---
+++ ReverseNumber.java
@@ -0,0 +1,17 @@
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+
+        int num = 12345;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        System.out.println("Original Number : 12345");
+        System.out.println("Reversed Number : " + reverse);
+    }
+}
